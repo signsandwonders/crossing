7cc6f2329236050a133290eb2004ac668a44338e
signsandwonders <73362951+signsandwonders@users.noreply.github.com>
2020-10-25 16:14:00 -0600

    Added automatic switch when the light level on the Micro:bit cuts in half

--- main.ts
+++ main.ts
@@ -2,15 +2,24 @@ input.onButtonPressed(Button.A, function () {
     raiseArm()
 })
 function raiseArm () {
-    pins.servoWritePin(AnalogPin.P0, 90)
+    pins.servoWritePin(AnalogPin.P15, 90)
+    basic.showIcon(IconNames.Happy)
 }
 input.onButtonPressed(Button.B, function () {
     lowerArm()
 })
 function lowerArm () {
-    pins.servoWritePin(AnalogPin.P0, 0)
+    pins.servoWritePin(AnalogPin.P15, 0)
+    basic.showIcon(IconNames.No)
 }
+let lightlevel = 1
 basic.showIcon(IconNames.No)
 basic.forever(function () {
-	
+    if (input.lightLevel() < lightlevel / 2) {
+        raiseArm()
+        basic.pause(5000)
+        lowerArm()
+    }
+    lightlevel = input.lightLevel()
+    basic.pause(1000)
 })
